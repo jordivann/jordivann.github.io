827fb9069ffe41fc4d4ece685153ca54cb110823
Jordi <jordievannorden@gmil.com>
2025-07-31 22:45:59 -0300

    deploy x 1000

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from './App.jsx';
+import './index.css';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
-    <App />
-
+      <App />
     </HashRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
