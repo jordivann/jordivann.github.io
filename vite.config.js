a60325b08edcd391a4e76ae3c05d8e19409de43b
Jordi <jordievannorden@gmil.com>
2025-07-31 21:57:44 -0300

    deploy?

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// reemplazá esto con el nombre de tu repositorio
+const repoName = 'jordivann.github.io';
+
 export default defineConfig({
+  base: `/`,
   plugins: [react()],
 });
